a71ce1c09df395a8afe8820a8916aaccd5c50f58
Paul Everitt <pauleveritt@me.com>
2017-12-11 15:56:50 -0500

    De-conflate the directive class and the window class.

--- game_api.py
+++ game_api.py
@@ -3,88 +3,91 @@ from inspect import signature
 import arcade
 
 
-class arcadeapi(arcade.Window):
-    _window = None  # This will hold instance of self as "global"
-    _game = None  # Class-based games register a game, we store instance
-
-    registry = dict(
-        setup=[],
-        update=[],
-        key_press=[],
-        draw=[],
-        game=None,
-        deferred_drawing=[]
-    )
-
-    def __init__(self, width: int, height: int, background_color=None):
+class ArcadeWindow(arcade.Window):
+    def __init__(self, registry: dict, width: int, height: int,
+                 background_color=None):
         super().__init__(width, height)
 
+        self.registry = registry
         self.width = width
         self.height = height
         if background_color:
             arcade.set_background_color(background_color)
 
-    @classmethod
-    def setup(cls):
-        for func in cls.registry['setup']:
-            if cls._game is not None:
+    def setup(self):
+        game = self.registry.get('game')
+        for func in self.registry['setup']:
+            if game is not None:
                 # Pass game instance as self
-                func(cls._game)
+                func(game)
             else:
                 sig = signature(func)
                 if 'window' in sig.parameters:
-                    func(cls._window)
+                    func(self)
                 else:
                     func()
 
-    @classmethod
-    def on_draw(cls):
+    def on_draw(self):
         arcade.start_render()
 
         # Process any deferred imperative drawing
-        for drawing in cls.registry['deferred_drawing']:
+        for drawing in self.registry['deferred_drawing']:
             this_cmd = getattr(arcade, drawing['cmd'])
             this_args = drawing['args']
             this_kwargs = drawing['kwargs']
             this_cmd(*this_args, **this_kwargs)
 
         # Now run registered handlers
-        for func in cls.registry['draw']:
-            if cls._game is not None:
+        game = self.registry.get('game')
+        for func in self.registry['draw']:
+            if game is not None:
                 # Pass game instance as self
-                func(cls._game)
+                func(game)
             else:
                 sig = signature(func)
                 if 'window' in sig.parameters:
-                    func(cls._window)
+                    func(self.registry['window'])
                 else:
                     func()
 
-    @classmethod
-    def update(cls, delta_time):
-        for func in cls.registry['update']:
-            if cls._game is not None:
+    def update(self, delta_time):
+        game = self.registry.get('game')
+        for func in self.registry['update']:
+            if game is not None:
                 # Pass game instance as self
-                func(cls._game, delta_time)
+                func(game, delta_time)
             else:
                 sig = signature(func)
                 if 'window' in sig.parameters:
-                    func(cls._window, delta_time)
+                    func(self.registry['window'], delta_time)
                 else:
                     func(delta_time)
 
-    @classmethod
-    def on_key_press(cls, key, key_modifiers):
-        for func in cls.registry['key_press']:
-            if cls._game is not None:
+    def on_key_press(self, key, key_modifiers):
+        game = self.registry.get('game')
+        for func in self.registry['key_press']:
+            if game is not None:
                 # Pass game instance as self
-                func(cls._game, key, key_modifiers)
+                func(game, key, key_modifiers)
             else:
                 sig = signature(func)
                 if 'window' in sig.parameters:
-                    func(cls._window, key, key_modifiers)
+                    func(self.registry['window'], key, key_modifiers)
                 else:
                     func(key, key_modifiers)
+
+
+class arcadeapi(arcade.Window):
+    registry = dict(
+        setup=[],
+        update=[],
+        key_press=[],
+        draw=[],
+        game_class=None,
+        game=None,  # Class-based games register a game, we store instance
+        window=None,  # This will pyglet window as "global"
+        deferred_drawing=[]
+    )
 
     # The decorators
     @classmethod
@@ -109,7 +112,7 @@ class arcadeapi(arcade.Window):
 
     @classmethod
     def game(cls, original_game):
-        cls.registry['game'] = original_game
+        cls.registry['game_class'] = original_game
 
     # Now re-implement the arcade drawing methods, to avoid use of
     # global window (for now, faking the re-implementation)
@@ -121,7 +124,7 @@ class arcadeapi(arcade.Window):
         # Imperative mode means no event-handler functions, simplest
         # possible mode. Detect this by seeing if run has been called. If
         # not, then defer this drawing.
-        if cls._window is None:
+        if cls.registry['window'] is None:
             cls.registry['deferred_drawing'].append(
                 dict(cmd='draw_text', args=args, kwargs=kwargs)
             )
@@ -130,7 +133,7 @@ class arcadeapi(arcade.Window):
 
     @classmethod
     def draw_circle_filled(cls, *args, **kwargs):
-        if cls._window is None:
+        if cls.registry['window'] is None:
             cls.registry['deferred_drawing'].append(
                 dict(cmd='draw_circle_filled', args=args, kwargs=kwargs)
             )
@@ -140,9 +143,13 @@ class arcadeapi(arcade.Window):
     @classmethod
     def run(cls, width: int = 600, height: int = 400,
             background_color=arcade.color.WHEAT):
-        cls._window = arcadeapi(width, height, background_color)
+        cls.registry['window'] = ArcadeWindow(
+            cls.registry, width, height,
+            background_color=background_color)
+
         # If a game is registered, instantiate it
-        if cls.registry['game']:
-            cls._game = cls.registry['game'](cls._window)
-        cls.setup()
+        game_class = cls.registry.get('game_class')
+        if game_class:
+            cls.registry['game'] = game_class(cls.registry['window'])
+        cls.registry['window'].setup()
         arcade.run()
