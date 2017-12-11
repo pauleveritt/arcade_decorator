abab755f1dbdf83c989cfa2ac1897ced9296b36a
Paul Everitt <pauleveritt@me.com>
2017-12-11 16:17:30 -0500

    Simplify the simple game.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,4 @@
-"""
-
-Simplest possible game.
-
-"""
 from game_api import arcadeapi as arcade
 
 arcade.draw_text('Hello World', 10, 200, (0, 0, 0), 20)
-arcade.run(420, 240, background_color=(100, 100, 100))
+arcade.run()  # Or pass args to customize window
